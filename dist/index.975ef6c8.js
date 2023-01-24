6a96df61a20a70ddfc7c65a72f29f08845559987
Aleksey0808 <103374981+Aleksey0808@users.noreply.github.com>
2023-01-24 19:20:05 +0200

    add modules

--- dist/index.975ef6c8.js
+++ dist/index.975ef6c8.js
@@ -142,7 +142,7 @@
       this[globalName] = mainExports;
     }
   }
-})({"1RB6v":[function(require,module,exports) {
+})({"85bBE":[function(require,module,exports) {
 "use strict";
 var global = arguments[3];
 var HMR_HOST = null;
@@ -620,6 +620,6 @@ exports.export = function(dest, destName, get) {
     });
 };
 
-},{}]},["1RB6v","8lqZg"], "8lqZg", "parcelRequired7c6")
+},{}]},["85bBE","8lqZg"], "8lqZg", "parcelRequired7c6")
 
 //# sourceMappingURL=index.975ef6c8.js.map
